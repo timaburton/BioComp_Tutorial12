35b1294d3ea8e7b6355e59e3f3f3389a6ca20f8d
Lauren Klein <lklein1@nd.edu>
2020-11-03 16:01:27 -0500

    started from text file

--- Exercise10code.R
+++ Exercise10code.R
@@ -1,7 +1,14 @@
 data<-read.table("data.txt", header=TRUE, sep = ",", stringsAsFactors = FALSE)
 library(ggplot2)
 #Question 1
-
+#datasource: https://www.sheffield.ac.uk/mash/statistics/datasets Birthweight Dataset
+birthweight<-read.table("Question1data.txt", header=TRUE, sep = "\t", stringsAsFactors = FALSE)
+ggplot(data=birthweight, aes(x=mnocig, y=Birthweight))+
+  geom_point()+
+  xlab("Number Cigarettes Smoked by Mother")+
+  ylab("Birthweight of Baby (lb.)")+
+  stat_smooth(method="lm")+
+  theme_classic()
 
 ##Question 2
 ggplot(data=data, aes(x= region, y=observations))+
@@ -17,3 +24,6 @@ ggplot(data = data, aes(x=region, y=observations))+
   geom_jitter()+
   theme_classic()
 
+#Tell slightly different stories because it is easier to see the range/skew
+#of the data in the scatterplot. It is easier to see where data clusters 
+#around the mean in the scatterplot. 
