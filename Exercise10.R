744523830bad33e3f55d3e3e687e386a79aea4db
racheljennings <rjennin2@nd.edu>
2020-11-05 10:37:04 -0500

    Finished part 1 of exercise

--- Exercise10.R
+++ Exercise10.R
@@ -1,1 +1,11 @@
+# load data file
 dat <- read.csv("Angles.csv", header = TRUE, stringsAsFactors = FALSE)
+
+# load ggplot
+library(ggplot2)
+
+# create scatterplot and trend line
+ggplot(dat, aes(x=Incident.Angle, y=Refracted.Angle)) +
+  geom_point(color="blue") +
+  stat_smooth(method = "lm") +
+  theme_classic()
