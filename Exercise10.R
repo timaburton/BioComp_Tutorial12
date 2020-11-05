46f9eb9419d843a950fad3c9cc9f004424566210
racheljennings <rjennin2@nd.edu>
2020-11-05 10:41:58 -0500

    Added more comments to part 1

--- Exercise10.R
+++ Exercise10.R
@@ -1,11 +1,17 @@
+# part 1 of Exercise 10
+# create a scatter plot w a line of best fit for data on incident angle versus refracted angle
+
 # load data file
 dat <- read.csv("Angles.csv", header = TRUE, stringsAsFactors = FALSE)
 
 # load ggplot
 library(ggplot2)
 
-# create scatterplot and trend line
+# create scatter plot and trend line
 ggplot(dat, aes(x=Incident.Angle, y=Refracted.Angle)) +
   geom_point(color="blue") +
   stat_smooth(method = "lm") +
   theme_classic()
+
+#------------------------------------------------------------------
+
